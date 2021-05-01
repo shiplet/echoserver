681b2be4c59dce29c739ef3e79b32e450d477acf
shiplet <shiplet@protonmail.com>
2021-05-01 11:32:05 -0400

    convert to bufreader | log timestamp

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,10 @@
 use echoserver::ThreadPool;
 use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
+use chrono::prelude::*;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     let pool = ThreadPool::new(64);
 
@@ -10,21 +12,31 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         pool.execute(|| {
-            handle_connection(stream);
+            handle_connection(stream).unwrap();
         })
     }
 
     println!("Shutting down");
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1_048_576];
-    stream.read(&mut buffer).unwrap();
-    let req = std::str::from_utf8(&buffer).unwrap().replace("\u{0}", "");
-    println!("{} bytes", req.len());
-    println!("{}\n\n", req);
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+	let mut reader = BufReader::new(&mut stream);
+    let received: Vec<u8> = reader.fill_buf()?.to_vec();
+    reader.consume(received.len());
+
+    String::from_utf8(received)
+        .map(|msg| println!("{}\n{} bytes\n{}", Local::now(),msg.len(),msg))
+        .map_err(|err| {
+			std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+				format!("Unable to parse string to UTF-8: {}", err)
+            )
+        }).unwrap();
 
     let response = format!("{}", "HTTP/1.1 200 OK\r\n\n");
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
+    Ok(())
 }
